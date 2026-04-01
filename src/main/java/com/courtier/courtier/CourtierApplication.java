f0028e22dce8483e916bb31a169e1459f86e46ac
BeingShashwat <shashwatm878@gmail.com>
2026-04-01 20:14:00 +0530

    init spring boot project

---
+++ src/main/java/com/courtier/courtier/CourtierApplication.java
@@ -0,0 +1,13 @@
+package com.courtier.courtier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourtierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/courtier/courtier/CourtierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courtier.courtier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
